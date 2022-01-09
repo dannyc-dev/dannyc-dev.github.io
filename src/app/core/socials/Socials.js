41814b0e101f26f5a64546e6f9166fc5d4426053
MalwareSec <dannyc10@vt.edu>
2022-01-09 17:32:31 -0500

    tweaking

--- src/app/core/socials/Socials.js
+++ src/app/core/socials/Socials.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'; 
-import { ReactComponent as TwitterIcon} from '../../../assets/twitter.svg';
-import { ReactComponent as GithubIcon} from '../../../assets/github.svg';
-import { ReactComponent as LinkedinIcon} from '../../../assets/linkedin.svg';
-import { ReactComponent as TwitchIcon} from '../../../assets/twitch.svg';
+// import { ReactComponent as TwitterIcon} from '../../../assets/twitter.svg';
+// import { ReactComponent as GithubIcon} from '../../../assets/github.svg';
+// import { ReactComponent as LinkedinIcon} from '../../../assets/linkedin.svg';
+// import { ReactComponent as TwitchIcon} from '../../../assets/twitch.svg';
 import { ReactComponent as MenuIcon} from '../../../assets/burger.svg';
 import "./Socials.scss";
 import MobileGoHome from '../mobilegohome/MobileGoHome';
@@ -25,18 +25,23 @@ function Socials(props) {
                     <li>
                         <div className={!menuAnimation ? "menu-animate-back menu-anchor" : "menu-animate menu-anchor"} onClick={(e) => {handleChange(e); setMenuAnimation(!menuAnimation)}} onAnimationEnd={() => setMenuAnimation(menuAnimation)}><MenuIcon className="menu"/></div>
                     </li>
-                    <li>
-                        <a href="https://twitter.com/dannyc_dev" target="_blank" rel="noreferrer"><TwitterIcon className="icon twitter"/></a>
-                    </li>
-                    <li>
-                        <a href="https://www.linkedin.com/in/danny-colmenares/" target="_blank" rel="noreferrer"><LinkedinIcon className="icon linkedin"/></a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/dannyc-dev" target="_blank" rel="noreferrer"><GithubIcon className="icon"/></a>
-                    </li>
-                    <li>
-                        <a href="https://www.twitch.tv/dannyc_dev" target="_blank" rel="noreferrer"><TwitchIcon className="icon twitch"/></a>
-                    </li>
+                    {menuAnimation &&
+                    <>
+                        {/* <li>
+                            <a href="https://twitter.com/dannyc_dev" target="_blank" rel="noreferrer"><TwitterIcon className="icon twitter"/></a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/danny-colmenares/" target="_blank" rel="noreferrer"><LinkedinIcon className="icon linkedin"/></a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/dannyc-dev" target="_blank" rel="noreferrer"><GithubIcon className="icon"/></a>
+                        </li>
+                        <li>
+                            <a href="https://www.twitch.tv/dannyc_dev" target="_blank" rel="noreferrer"><TwitchIcon className="icon twitch"/></a>
+                        </li> */}
+                    </>
+                    }
+                    
                 </ul>
             </div>
         </div>
