bd317e2b6a38d757cc364d7871816a4741a62867
MalwareSec <dannyc10@vt.edu>
2022-01-09 22:18:55 -0500

    fix warning

--- src/app/route-book/book-preview/BookPreview.js
+++ src/app/route-book/book-preview/BookPreview.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router-dom'
 import { useMediaQuery } from 'react-responsive';
 import { CSSTransition } from 'react-transition-group';
