f1bd985a6ee6db6b51a6b831d048153610a283f3
MalwareSec <dannyc10@vt.edu>
2022-01-27 23:31:48 -0500

    updates

--- src/app/route-blog/barena/Barena.js
+++ src/app/route-blog/barena/Barena.js
@@ -46,12 +46,12 @@ function Barena(props) {
             </div>
             </CSSTransition>
             <div className="blog-p">
-            Diving into the foundations of reading and writing to process memory externally. To do this, I set up two applications. A main game application and an external trainer. For those not familiar with <a target="_blank" rel="noreferrer" href="https://en.wikipedia.org/wiki/Trainer_(games)#:~:text=Game%20trainers%20are%20programs%20made,in%20order%20to%20allow%20cheating.">game trainers</a>, the term originated sometime in the 80's and can be loosely defined as a program that is able to modify a different program's memory in order to enable new features or functionality. Below you'll see an example of the two programs running side by side. On the left the external trainer, showing process details, a menu of possible options, and an example of changing the player stats. On the right side our game is running, showing the main menu of our game lobby and then showing updated player values after we've modified them in our trainer.
+            I dove into the foundations of reading and writing externally to process memory by creating two prototypes. A main game application and an external trainer. For those not familiar with <a target="_blank" rel="noreferrer" href="https://en.wikipedia.org/wiki/Trainer_(games)#:~:text=Game%20trainers%20are%20programs%20made,in%20order%20to%20allow%20cheating.">game trainers</a>, the term originated sometime in the 80's and can be loosely defined as a program that is able to modify a different program's memory in order to enable new features or functionality. Below you'll see an example of the two programs running side by side. On the left the external trainer, showing process details, a menu of possible options, and an example of changing the player stats. On the right side our game is running, showing the main menu of our game lobby and then showing updated player values after we've modified them in our trainer.
             </div>
             <div className="blog-image-container">
                 <img className="blog-image-l" alt="preview" src={PreviewGif}/>
             </div>
-            <div className="blog-sub-header">The Tech Stuff</div>
+            <div className="blog-sub-header">The Tech Details</div>
             <div className="blog-p">
                 <div className="blog-center" >
                     Github Links to repos:&nbsp;
@@ -63,7 +63,7 @@ function Barena(props) {
                         Battle Arena Game
                     </a>
                 </div>
-                These projects were built in Visual Studio 2022 on Windows 11 v10.0.22000 (sorry Mac fans, Windows only for this one). To download and compile from source, open the solution file of each project in visual studio, target your release, and build the project or head over to the github release pages and download the binaries directly - <a target="_blank" rel="noreferrer" href="https://github.com/dannyc-dev/Battle-Arena-In-Memory/releases/">Battle Arena Game Releases</a>, and <a target="_blank" rel="noreferrer" href="https://github.com/dannyc-dev/Battle-Arena-Trainer/releases/">Battle Arena Trainer Releases</a>.
+                These projects were built in Visual Studio 2022 on Windows 11 v10.0.22000 (sorry Mac fans, Windows only for this one). To download and compile from source, open the solution file of each project in visual studio, target your release, and build the project. Alternatively, you can head over to the github release pages for each project and download the precompiled binaries - <a target="_blank" rel="noreferrer" href="https://github.com/dannyc-dev/Battle-Arena-In-Memory/releases/">Battle Arena Game Releases</a>, and <a target="_blank" rel="noreferrer" href="https://github.com/dannyc-dev/Battle-Arena-Trainer/releases/">Battle Arena Trainer Releases</a>.
 
                 <b> Note:</b> Downloading and running the binaries directly will give you scary warnings about downloading and running untrusted and unsigned code off the internet. And I honestly agree with them, but the option is available.
 
@@ -73,16 +73,20 @@ function Barena(props) {
                 <div className="blog-list-container" >
                 <div className="blog-list-header">Tooling Specs:</div>
                     <ul className="blog-list">
-                        <li>Visual Studio 2022</li>
-                        <li>IDA Home</li>
-                        <li>Cheat Engine</li>
+                        <li>Visual Studio 2022 (aside from some project properties, it probably doesn't really matter which version you have)</li>
+                        <li>IDA Home (free version also works)</li>
+                        <li>Cheat Engine (optional but it wouldn't be game hacking post without mentioning it at least once)</li>
                     </ul>
                 </div>
             </div>
-            <div className='blog-sub-header'>The Fun Stuff</div>
-            <div className='blog-p'>
-                Text
+            <div className='blog-sub-header'>Battle Arena - Game Design</div>
+            <div className="blog-image-container">
+                <img className="blog-image-l" alt="preview" src={"https://dannyc-assets.s3.amazonaws.com/d3d-sim/design.jpeg"}/>
             </div>
+            <div className='blog-p'>
+                I tried to design the simplest game I could while still giving the player some control over key game loop inputs. Each turn (or tick), the player is faced with a decision to attack the opponent or try to heal themselves with a potion. The enemy player will mirror whatever you decide, so choose wisely. Although the rules of the game are simple, it gives us just enough control to make altering the game state interesting. Let's get to the fun stuff.
+            </div>
+            <div className='blog-sub-header'>"The Fun Stuff"</div>
         </div>
     )
 }
